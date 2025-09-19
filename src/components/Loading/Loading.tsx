92f97c4b819f6ee6e628c00411665d513ec13fbc
Yousif Al-Raheem <yousifalraheem@gmail.com>
2025-09-19 17:32:55 +0200

    normalize quotes in Loading component

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,14 +1,14 @@
-import { Box, CircularProgress } from "@material-ui/core";
-import type { FC } from "react";
+import { Box, CircularProgress } from '@material-ui/core';
+import type { FC } from 'react';
 
 export const Loading: FC = () => (
   <Box
     sx={{
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      width: "100%",
-      height: "50vh",
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: '100%',
+      height: '50vh',
     }}
   >
     <CircularProgress size="1.5em" />
