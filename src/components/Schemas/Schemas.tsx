0255623862b5d881b3f17084f0d2319ccc536073
Yousif Al-Raheem <yousifalraheem@gmail.com>
2025-09-19 17:38:02 +0200

    normalize quotes in Schemas component

--- src/components/Schemas/Schemas.tsx
+++ src/components/Schemas/Schemas.tsx
@@ -1,23 +1,23 @@
-import { type FC, useEffect } from 'react';
-import AceEditor from 'react-ace';
-import 'ace-builds/src-noconflict/mode-java';
-import 'ace-builds/src-noconflict/theme-github';
-import 'brace';
-import 'brace/ext/language_tools';
-import 'brace/ext/searchbox';
-import 'brace/theme/chrome';
-import 'brace/mode/yaml';
-import 'brace/mode/json';
-import 'brace/snippets/yaml';
-import 'brace/snippets/json';
-import 'swagger-ui-react/swagger-ui.css';
-import { Box, Typography } from '@material-ui/core';
-import { Violations } from '../Violations';
-import type { ViolationsResponse } from '../../api/types';
-import LinkIcon from '@material-ui/icons/Link';
-import SwaggerUI from 'swagger-ui-react';
-import * as S from './styles';
-import type { ICommonEventInfo, IEventTracking } from '../../event-types';
+import { type FC, useEffect } from "react";
+import AceEditor from "react-ace";
+import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/theme-github";
+import "brace";
+import "brace/ext/language_tools";
+import "brace/ext/searchbox";
+import "brace/theme/chrome";
+import "brace/mode/yaml";
+import "brace/mode/json";
+import "brace/snippets/yaml";
+import "brace/snippets/json";
+import "swagger-ui-react/swagger-ui.css";
+import { Box, Typography } from "@material-ui/core";
+import { Violations } from "../Violations";
+import type { ViolationsResponse } from "../../api/types";
+import LinkIcon from "@material-ui/icons/Link";
+import SwaggerUI from "swagger-ui-react";
+import * as S from "./styles";
+import type { ICommonEventInfo, IEventTracking } from "../../event-types";
 
 type SchemasProps = {
   onInputChange: (value: string) => void;
@@ -52,17 +52,17 @@ export const Schemas: FC<SchemasProps> = ({
     openUrlDialog();
     sendEvent?.({
       ...(event as ICommonEventInfo),
-      eventLabel: 'onClick to import URL link',
-      eventAction: 'Clicks on import URL link',
+      eventLabel: "onClick to import URL link",
+      eventAction: "Clicks on import URL link",
     });
   };
 
   let mode;
   try {
     JSON.parse(schemaValue as string);
-    mode = 'json';
+    mode = "json";
   } catch (e) {
-    mode = 'yaml';
+    mode = "yaml";
   }
 
   useEffect(() => {
@@ -116,8 +116,8 @@ export const Schemas: FC<SchemasProps> = ({
             onFocus={() =>
               sendEvent?.({
                 ...(event as ICommonEventInfo),
-                eventLabel: 'onFocus on Schema Editor input',
-                eventAction: 'focus on Schema Editor input',
+                eventLabel: "onFocus on Schema Editor input",
+                eventAction: "focus on Schema Editor input",
               })
             }
             enableBasicAutocompletion
